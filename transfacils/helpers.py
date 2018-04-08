8144b0655ab57c3e2978628d38fb17f2fe38c549
kkiyama117 <k.kiyama117@gmail.com>
2018-04-08 14:50:10 +0900

    Add line_data_generator in helpers

--- transfacils/helpers.py
+++ transfacils/helpers.py
@@ -2,13 +2,16 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def trans_data_generator(prefecture_id=26):
-    url = "http://www.ekidata.jp/api/p/{0}.xml".format(prefecture_id)
+def gen_helper(url):
     # Requestsを使って、webから取得
     r = requests.get(url)
     r.encoding = r.apparent_encoding
-    bs = BeautifulSoup(r.text, "lxml-xml")
-    lines = bs.find_all("line")
+    return BeautifulSoup(r.text, "lxml-xml")
+
+
+def lines_data_generator(prefecture_id=26):
+    url = "http://www.ekidata.jp/api/p/{0}.xml".format(prefecture_id)
+    lines = gen_helper(url).find_all("line")
     for line in lines:
         line_cd = line.find("line_cd").string
         line_name = line.find("line_name").string
@@ -16,6 +19,14 @@ def trans_data_generator(prefecture_id=26):
                "line": line_name}
 
 
+def line_data_generator(line_id=33001):
+    url = "http://www.ekidata.jp/api/l/{0}.xml".format(line_id)
+    line = gen_helper(url)
+    stations = line.find_all("station")
+    for station in stations:
+        yield {"station_name": station.find("station_name").string}
+
+
 if __name__ == '__main__':
-    for i in trans_data_generator():
+    for i in line_data_generator():
         print(i)

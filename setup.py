443a5c07839c0804fae8d3188ded63e4ce87382c
kkiyama117 <k.kiyama117@gmail.com>
2018-04-28 16:55:16 +0900

    Update requirements

--- setup.py
+++ setup.py
@@ -3,15 +3,14 @@ from setuptools import setup
 
 setup(
     name='mainHP',
-    version='0.3',
-    description='Household account book in times of day format',
+    version='0.4.1',
+    description='kkiyama117\'s homepage',
+    author='kkiyama117',
+    author_email='k.kiyama117@gmail.com',
     packages=['lib', 'lib.tests', 'config', 'account', 'account.tests',
               'account.management', 'account.management.commands',
               'tweetable', 'tweetable.tests',
               'transfacils', 'transfacils.tests', 'transfacils.helpers', ],
     url='http://hinatan.jp',
     license='',
-    author='kkiyama117',
-    author_email='k.kiyama117@gmail.com',
-    description='My first server'
 )

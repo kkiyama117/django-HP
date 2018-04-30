68a2025907b7e6ae2392203ec3f8fbc69f7821c2
kkiyama117 <k.kiyama117@gmail.com>
2018-05-01 08:43:06 +0900

    Update models

--- unimap/models.py
+++ unimap/models.py
@@ -1,9 +1,15 @@
 from django.db import models
 
 
-class University(models.Model):
+class Place(models.Model):
     name = models.CharField(max_length=256)
-    slug = models.SlugField(max_length=256)
+
+    class Meta:
+        abstract = True
 
     def __str__(self):
         return self.name
+
+
+class University(Place):
+    slug = models.SlugField(max_length=256)

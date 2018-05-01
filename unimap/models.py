99d4f4cab1d9a8bfbfcff45fb80a351b9d330be9
kkiyama117 <k.kiyama117@gmail.com>
2018-05-02 00:16:56 +0900

    Add models and test

--- unimap/models.py
+++ unimap/models.py
@@ -1,10 +1,14 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 from model_utils.managers import InheritanceManager
 
 
 class Place(models.Model):
-    name = models.CharField(max_length=256)
+    name = models.CharField(max_length=256, null=False, blank=False,
+                            unique=True)
     objects = InheritanceManager()
+    latitude = models.FloatField(null=False)
+    longitude = models.FloatField(null=False)
 
     class Meta:
         abstract = True
@@ -14,4 +18,25 @@ class Place(models.Model):
 
 
 class University(Place):
+    """大学を表すクラス"""
     slug = models.SlugField(max_length=256)
+
+
+class Campus(Place):
+    """キャンパスを表すクラス"""
+    name = models.CharField(max_length=256)
+    university = models.ForeignKey(University, on_delete=models.CASCADE)
+    group = models.CharField(max_length=128, default=name)
+
+
+class Building(Place):
+    """建物を表すクラス"""
+    campus = models.ForeignKey(Campus, on_delete=models.CASCADE)
+
+
+class Room(Place):
+    """部屋を表す"""
+    building = models.ForeignKey(Building, on_delete=models.CASCADE)
+    # Validator はmodelformを使うときだけ
+    floor = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(10)])
--- unimap/tests/test_model.py
+++ unimap/tests/test_model.py
@@ -1,7 +1,8 @@
 import pytest
 from django.core.exceptions import ValidationError
 
-from unimap.models import University
+from unimap.factories import RoomFactory
+from unimap.models import University, Room
 
 
 class UniversityTest:
@@ -12,7 +13,8 @@ class UniversityTest:
     ])
     @pytest.mark.django_db
     def test_create_instance(self, name, slug):
-        univ = University(id=1, name=name, slug=slug)
+        univ = University(id=1, name=name, slug=slug, latitude=0.0,
+                          longitude=0.0)
         univ.save()
         assert univ.name == name
         assert univ.slug == slug
@@ -25,9 +27,29 @@ class UniversityTest:
     ])
     @pytest.mark.django_db
     def test_assertion(self, name, slug):
-        univ = University(id=0, name=name, slug=slug)
+        univ = University(id=0, name=name, slug=slug, latitude=0.0,
+                          longitude=0.0)
         try:
             univ.full_clean()
+        except ValidationError as e:
+            pytest.fail()
+            print(e)
+
+
+class RoomTest:
+    @pytest.mark.django_db
+    def test_create_instance(self):
+        room = RoomFactory()
+        assert room.name == "Kyokita22"
+        assert room.floor == 2
+        assert str(room) == room.name
+        assert Room.objects.count() == 1
+
+    @pytest.mark.django_db
+    def test_assertion(self):
+        room = RoomFactory()
+        try:
+            room.full_clean()
         except ValidationError as e:
             pytest.fail()
             print(e)
